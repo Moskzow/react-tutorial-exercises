0d8f36af48c0c87bfdd186667f001f8876c81f5e
Moskzow <inmoskwetrust@gmail.com>
2021-05-07 22:29:56 +0000

    exercise 1.5 done

--- exercises/01.4-building-a-layout/index.js
+++ exercises/01.4-building-a-layout/index.js
@@ -22,7 +22,7 @@ let content = (
 		<img className="card-img-top" src={data.image} alt="Card image cap" />
 		<div className="card-body">
 			<h5 className="card-title">{data.cardTitle}</h5>
-            <p className="card-text">{data.cardDescription}</p>
+			<p className="card-text">{data.cardDescription}</p>
 			<a href={data.button.url} className="btn btn-primary">
 				{data.button.label}
 			</a>
